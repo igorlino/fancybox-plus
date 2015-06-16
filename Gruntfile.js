eccbf5fea08c72311f11d00ac7492634d12e37e5
Igor Lino <i.lino@directica.pl>
2015-06-17 00:16:36 +0200

    Initial commit based on fancybox 1.3.4

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        // Import package manifest
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Banner definitions
+        meta: {
+            banner: '/*\n' +
+            ' *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+            ' *  <%= pkg.description %>\n' +
+            ' *  <%= pkg.homepage %>\n' +
+            ' *\n' +
+            ' *  Made by <%= pkg.author.name %>\n' +
+            ' *  Under <%= pkg.license %> License\n' +
+            ' */\n'
+        },
+
+        // Concat definitions
+        concat: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                src: ['src/jquery.fancybox-plus.js'],
+                dest: 'dist/jquery.fancybox-plus.js'
+            }
+        },
+
+        // Lint definitions
+        jshint: {
+            files: ['src/jquery.fancybox-plus.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        // JSCS definitions
+        jscs: {
+            src: 'src/*.js',
+            options: {
+                config: '.jscsrc',
+                esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext
+                verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+                requireCurlyBraces: ['if']
+            }
+        },
+
+        // Minify definitions
+        uglify: {
+            my_target: {
+                src: ['dist/jquery.fancybox-plus'],
+                dest: 'dist/jquery.fancybox-plus.min.js'
+            },
+            options: {
+                banner: '<%= meta.banner %>'
+            }
+        }
+
+        // watch for changes to source
+        // Better than calling grunt a million times
+        // (call 'grunt watch')
+        watch: {
+            files: ['src/*'],
+            tasks: ['default']
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['concat', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'jscs', 'build']);
+    grunt.registerTask('travis', ['default']);
+
+};
---
+++ README.md
@@ -0,0 +1,40 @@
+[jQuery FancyBox-Plus Plugin](http://igorlino.github.io/fancybox-plus/)
+================================
+
+[EZ Plus](http://igorlino.github.io/fancybox-plus/) is an up-to-date jQuery plug-in based on the [FancyBox 1.3](https://github.com/elevateweb/elevatezoom)
+
+## Features
+
+- Modal
+- Easing
+- JQuery plug-in
+
+## Installation
+
+Via [npm](https://www.npmjs.com/):
+
+```bash
+npm install fancybox-plus
+```
+
+Via [Bower](http://bower.io/):
+
+```bash
+bower install fancybox-plus
+```
+
+In a browser:
+
+```html
+<script src="jquery.fancybox-plus.js"></script>
+```
+
+## Getting Started
+
+Include jQuery and the plug-in on a page. Include your images and initialise the plug-in.
+
+
+For more information on how to setup and customise, [check the examples](http://igorlino.github.io/fancybox-plus/).
+
+## License
+Licensed under MIT license.
